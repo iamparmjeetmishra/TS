955e90eff88a7b88d4a316ac5157db277e3782d5
Parmjeet <parmjeetmishra2@gmail.com>
2023-12-28 00:07:20 +0530

    Lesson: 01 Type Annotations

---
+++ 00Basics/01TypeAnnotations.ts
@@ -0,0 +1,21 @@
+// Strings
+// Numbers
+// Boolean
+
+
+// Strings
+
+// let myName: string = 12 // Error
+let myName: string = 'John'
+myName = 'wick'
+
+console.log(myName)
+
+// Numbers
+
+let favNum: number = 7
+console.log(favNum)
+
+// Boolean
+let tsHard: boolean = false
+console.log(tsHard)
